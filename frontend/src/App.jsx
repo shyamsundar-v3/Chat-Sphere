09e7ffcf5f3e2ded9d646ae65695e3eee71be3ac
Shyam Sundar <shyamsunder4689@gmail.com>
2026-04-19 10:47:08 +0530

    Set up Chakra UI and application routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { Container,Box } from "@chakra-ui/react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Header from './components/Header';
+import HomePage from "./pages/HomePage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import { useRecoilValue } from "recoil";
+import userAtom from "./atoms/userAtom";
+import UpdateProfilePage from "./pages/updateProfilePage.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+function App() {
+  const user = useRecoilValue(userAtom);
+  return (
+  <>
+  
+    <Box position={"relative"} w='full'>
+    <Container maxW={ "620px"}>
+    <Header />
+    
+     <Routes>
+     <Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
+     <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
+     <Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
+      <Route path='/:username' element={ user ? (
+        <>
+        <UserPage />
+        <CreatePost />
+        </>
+      ) : (
+         <UserPage />
+      )}/>
+      <Route path='/:username/post/:pid' element={<PostPage />}/>
+      <Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />}/>
+     </Routes>
+
+      
+   
+     </Container>
+    </Box>
+   
+    </>
+  );
+}
+
+export default App;
